35c5249410dde9ee20581ef73c99bd98a9974a47
Eric Pratt <ericprattusc@gmail.com>
2023-06-08 17:36:22 -0700

    first stuff

---
+++ game.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const strings = [1,2,3,4,5,6];
+    const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A#', 'C#', 'D#', 'F#', 'G#', 'Ab', 'Bb', 'Db', 'Eb', 'Gb'];
+    
+})
