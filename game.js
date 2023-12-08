74fb5bf0cb6e310daf08ad4357af793ac7092cac
Eric Pratt <ericprattusc@gmail.com>
2023-12-08 14:57:12 -0800

    buttons added and changing note and string based on click

--- game.js
+++ game.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', function(){
     const strings = [1,2,3,4,5,6];
     const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A#', 'C#', 'D#', 'F#', 'G#', 'Ab', 'Bb', 'Db', 'Eb', 'Gb'];
+    const scoreElement = document.getElementById('score');
+    const increment = document.getElementById('increment');
+
+    let score = 0; 
+
+    increment.addEventListener('click', function(){
+        score+=5; 
+        scoreElement.innerHTML = score;
+        const stringValue = document.getElementById('num');
+        const noteValue = document.getElementById('note'); 
+        stringValue.innerHTML = '5';
+        noteValue.innerHtml = 'C';
+
+    });
     
 })
+
