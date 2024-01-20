e5cade1632142912d1822ff9f3d5205cab2dce7f
Duojet2ez <epstringskip@gmail.com>
2024-01-19 19:33:42 -0800

    express added

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
 const http = require('http');
+const express = require('express');
+const app = express();
 const {readFileSync} = require('fs'); 
 const fs = require('fs');  
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const homePage = readFileSync('./index.html');
-const scriptLoad = readFileSync('./game.js'); 
-const style = readFileSync('./style.css'); 
 
-const server = http.createServer((req, res) => {
-    const url = req.url
-    if(url === '/'){
-        res.writeHead(200, {'content-type':'text/html'}); 
-        res.write(homePage); 
-        console.log('home page found');
-        res.end(); 
-    }
-    else if(url === '/style.css'){
-        console.log('stylesheet found'); 
-        res.writeHead(200, {'content-type':'text/css'});
-        res.write(style);
-        res.end(); 
-    }
-    else if(url === '/game.js'){
-        console.log('script found'); 
-        res.writeHead(200, {'content-type':'text/javascript'});
-        res.write(scriptLoad);
-        res.end();
-    }
+app.use(express.static('./'));
 
-});
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+
