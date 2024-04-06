7639db942feca2a61cfb56f1a06d0e75dedd4d17
Duojet2ez <epstringskip@gmail.com>
2024-04-06 15:48:07 -0700

    auto correlation importing

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
-app.use(express.static('./'));
-
+// Serve static files from the root directory
+app.use(express.static(__dirname));
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-
+  console.log(`Example app listening on port ${port}`);
+});
--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+import { autoCorrelate } from "./autoCorrelationAlgo.js";
+
 document.addEventListener('DOMContentLoaded', function(){
     const strings = [1,2,3,4,5,6];
     const notes = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'A#', 'C#', 'D#', 'F#', 'G#', 'Ab', 'Bb', 'Db', 'Eb', 'Gb'];
